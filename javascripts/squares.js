42acabc5d34a257f2a49e1cd6137c070bc77a593
Alex Konrad <alexkonrad08@gmail.com>
2014-03-25 16:02:03 -0400

    clean up javascript more

--- javascripts/squares.js
+++ javascripts/squares.js
@@ -1,11 +1,11 @@
 (function(root) {
     var Squares = root.Squares = (root.Squares || {})
 
-    var WINDOW_WIDTH = 900;
-    var WINDOW_HEIGHT = 700;
-    var TILE_WIDTH = 100;
-    var TILE_HEIGHT = 100;
-    var MIN_SPEED = 1;
+    var WINDOW_WIDTH = 400;
+    var WINDOW_HEIGHT = 400;
+    var TILE_WIDTH = 50;
+    var TILE_HEIGHT = 50;
+    var MIN_SPEED = .5;
     var squares = Squares.squares = [];
     var $section = undefined;
     var $fragment = document.createDocumentFragment();
@@ -26,8 +26,8 @@
 
         this._checkBounds();
 
-        newPos[0] = curPos[0] + (this._dir[0] * this._speed[0]);
-        newPos[1] = curPos[1] + (this._dir[1] * this._speed[1]);   
+        newPos[0] = curPos[0] + (this._dir[0] * (2 * this._speed[0]));
+        newPos[1] = curPos[1] + (this._dir[1] * (2 * this._speed[1]));   
 
         this._setPos(newPos);
     }
@@ -43,49 +43,58 @@
 
     Square.prototype._checkBounds = function () {
         var currentPos = this._getPos();
-        var borderRight = WINDOW_WIDTH - parseInt(this._el.style.width);
-        var borderBottom = WINDOW_HEIGHT - parseInt(this._el.style.height);
+        var borderRight = WINDOW_WIDTH - TILE_WIDTH;
+        var borderBottom = WINDOW_HEIGHT - TILE_HEIGHT;
 
         // reverse direction if necessary
         if (currentPos[0] < 0 || currentPos[0] > borderRight) {
             this._dir[0] *= -1;
-        } else if (currentPos[1] < 0 || currentPos[1] > borderBottom) {
+        } else if (currentPos[1] < 0) {
             this._dir[1] *= -1;
+        } else if (currentPos[1] > borderBottom) {
+            this._dir[1] *= -1;
+            this._speed[1] = MIN_SPEED + Math.random();
         }
     }
 
     window.onload = function () {
+        WINDOW_WIDTH = document.body.offsetWidth;
+        WINDOW_HEIGHT = document.body.offsetHeight;
         $section = document.querySelector('section');
 
         document.styleSheets[0].insertRule('article { width: ' + TILE_WIDTH + '; height: ' + TILE_HEIGHT + ' }', 0);
 
-        addSquare();
-        renderSquares();
+        _addSquare(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2);
+        _renderSquares();
 
         document.querySelector('section').addEventListener(
             'mousedown',
             function(event) {
                 event.preventDefault();
-                if(event.target.tagName.toLowerCase() == 'article'){ addSquare(); }
+                if(event.target.tagName.toLowerCase() === 'article'){
+                    for (var i = 0; i < 5; i++) {
+                        _addSquare(event.pageX, event.pageY);
+                    }
+                }
             },
             false
         );
     };
-
-    function addSquare() {
+    
+    function _addSquare(x, y) {
         function sign () { 
             return Math.random() < .5 ? -1 : 1; 
         }
         squares.push(new Square({
-            x : Math.random() * (WINDOW_WIDTH - TILE_WIDTH),
-            y : Math.random() * (WINDOW_HEIGHT - TILE_HEIGHT),
+            x : x,
+            y : y,
             color: '#'+(Math.random()*0xFFFFFF<<0).toString(16),
             speed: [MIN_SPEED + Math.random(), MIN_SPEED + Math.random()], 
             dir: [sign(), sign()]
         }));
     }
 
-    function renderSquares() {
+    function _renderSquares() {
         while($section.hasChildNodes()) {
             $section.removeChild($section.firstChild);
         }
@@ -96,6 +105,6 @@
         }); 
 
         $section.appendChild($fragment.cloneNode(true));
-        requestAnimationFrame(renderSquares);
+        requestAnimationFrame(_renderSquares);
     }
 })(this);
